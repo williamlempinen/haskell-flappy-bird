c6369e1581fc61266b2d2d5cfe21c9b1b07b4804
William Lempinen <wlempin@gmail.com>
2024-03-28 18:47:51 +0200

    game over UI

--- src/UI.hs
+++ src/UI.hs
@@ -13,7 +13,7 @@ window = InWindow "Flappy Bird" (1800, 1000) (500, 500)
 loadBgImage :: IO Picture
 loadBgImage = fmap (scale 2 2) (loadBMP "src/assets/flappy_bird_bg.bmp")
 
---draw static elements
+--draw elements
 drawGround :: Picture
 drawGround = translate 0 (-500) (rectangleSolid 1800 10)
 
@@ -24,10 +24,19 @@ drawBird :: Bird -> Picture
 drawBird (Bird (xPos, yPos) _) = translate xPos yPos (circleSolid 40)
 
 drawScore :: Int -> Picture
-drawScore score = translate (-150) (-400) (scale 0.5 0.5 (color black (Text ("Score: " ++ show score))))
+drawScore score = Pictures [translate 0 (-400) (scale 0.5 0.5 (color black (Text (show score)))),
+                            translate 1 (-400) (scale 0.5 0.5 (color black (Text (show score)))),
+                            translate (-1) (-400) (scale 0.5 0.5 (color black (Text (show score)))),
+                            translate 0 (-401) (scale 0.5 0.5 (color black (Text (show score)))),
+                            translate 0 (-399) (scale 0.5 0.5 (color black (Text (show score))))]
 
 drawGameOver :: Picture
-drawGameOver = translate 0 0 (circleSolid 60)
+drawGameOver = Pictures [translate (-380) 100 (color (dark black) (Text "GAME OVER")), 
+                         translate (-381) 101 (color (dark black) (Text "GAME OVER")),
+                         translate (-379) 99 (color (dark black) (Text "GAME OVER")),
+                         translate (-380) 101 (color (dark black) (Text "GAME OVER")),
+                         translate (-380) 99 (color (dark black) (Text "GAME OVER")),
+                         translate (-420) (-180) (scale 0.5 0.5(color (dark black) (Text "PRESS ENTER TO CONTINUE")))]
 
 drawMenu :: Picture
 drawMenu = Pictures [translate (-400) 100 (color (dark black) (Text "FLAPPY BIRD")), 
